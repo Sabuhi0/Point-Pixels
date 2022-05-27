141cebbd03f487f8b80b8e38767ff14bb1a197c7
sabuhiq0@gmail.com <sabuhiq0@gmail.com>
2022-05-27 20:18:05 +0400

    Work template js

--- src/assets/js/works.js
+++ src/assets/js/works.js
@@ -7,3 +7,20 @@ document.querySelector(".header-content").onclick = () => {
 document.querySelector("#aside-close").onclick = () => {
     document.querySelector(".works-aside-section").classList.remove('active');
 }
+
+
+const works_items = document.querySelectorAll(".works-items");
+works_items.forEach(item => {
+    if(item){
+        item.addEventListener('click', () => {
+            document.querySelector(".work-template").classList.add('active');
+      });
+    }  
+})
+
+const works_aside_close = document.querySelector(".works-aside-close");
+if(works_aside_close){
+    works_aside_close.addEventListener('click', () => {
+        document.querySelector(".work-template").classList.remove('active');
+  });
+}
