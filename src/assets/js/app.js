236f44a9334173196e0870eacf8786d9a8944001
sabuhiq0@gmail.com <sabuhiq0@gmail.com>
2022-05-30 15:39:47 +0400

    Add app.js file

---
+++ src/assets/js/app.js
@@ -0,0 +1,65 @@
+// Aside menu
+
+const aside_section_button = document.querySelector(".aside-section-button");
+if(aside_section_button) {
+    aside_section_button.addEventListener("click", () => {
+        document.querySelector(".aside-section").classList.add('active');
+    })
+}
+
+const aside_close = document.querySelector(".aside-close");
+if(aside_close) {
+    aside_close.addEventListener("click", () => {
+        document.querySelector(".aside-section").classList.remove('active');
+    })
+}
+
+// document.querySelector(".aside-section-button").onclick = () => {
+//     document.querySelector(".aside-section").classList.add('active');
+// }
+
+// document.querySelector(".aside-close").onclick = () => {
+//     document.querySelector(".aside-section").classList.remove('active');
+// }
+
+
+
+// Maousehover
+
+const work = (e) => {
+    e.addEventListener("mouseover", () => {
+        isMouseHover = true
+        document.querySelector(".aside-content-image").classList.add('show');
+    });
+    e.addEventListener("mouseleave", () => {
+        isMouseHover = false
+        document.querySelector(".aside-content-image").classList.remove('show');
+    });
+}
+
+const contact = (e) => {
+    e.addEventListener("mouseover", () => {
+      isMouseHover = true
+      document.querySelector(".aside-content").classList.add('show');
+    });
+    e.addEventListener("mouseleave", () => {
+        isMouseHover = false
+        document.querySelector(".aside-content").classList.remove('show');
+    });
+}
+
+// Works
+
+const header_content = document.querySelector(".header-content");
+if(header_content) {
+    header_content.addEventListener("click", () => {
+        document.querySelector(".works-aside-section").classList.add('active');
+    })
+}
+
+const works_aside_close = document.querySelector("#aside-close");
+if(works_aside_close) {
+    works_aside_close.addEventListener("click", () => {
+        document.querySelector(".works-aside-section").classList.remove('active');
+    })
+}
